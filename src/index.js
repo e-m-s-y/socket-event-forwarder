c43da8eaf331b209139e21348b53ea690d77cc68
e-m-s-y <hofman.gb@gmail.com>
2019-11-08 12:30:30 +0100

    added plugin logic

---
+++ src/index.js
@@ -0,0 +1,35 @@
+exports.plugin = {
+	pkg: require('../package.json'),
+	defaults: require('./defaults'),
+	alias: 'e-m-s-y:socket-event-forwarder',
+	io: undefined,
+	async register(container, options) {
+		const logger = container.resolvePlugin('logger');
+
+		if(options.events.length === 0) {
+			logger.info(`[${this.alias}] No events given, plugin will be disabled.`);
+
+			return;
+		}
+
+		try {
+			this.io = require('socket.io')(options.port);
+
+			const eventEmitter = container.resolvePlugin('event-emitter');
+
+			for(let event of options.events) {
+				eventEmitter.on(event, async(data) => {
+					this.io.emit(event, data);
+
+					logger.info(`[${this.alias}] Forwarded event ${event}.`);
+				});
+			}
+		} catch(error) {
+			logger.error(error);
+
+			// Ignore, don't let the relay crash.
+		}
+	}
+};
+
+
