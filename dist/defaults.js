c0a31eacaf5e016e752f0d3cb12d3932b8a367dd
e-m-s-y <hofman.gb@gmail.com>
2021-09-13 17:08:47 +0200

    WIP: skeleton

---
+++ dist/defaults.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+exports.defaults = void 0;
+exports.defaults = {
+    enabled: true,
+    port: 3000,
+    events: [],
+    confirmations: [],
+    customEvents: [],
+    systeminformationInterval: 5000,
+    networkLatencyInterval: 10000,
+    blockheightCurrentInterval: 10000
+};
