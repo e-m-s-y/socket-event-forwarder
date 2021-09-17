f5a288939b385f22b96ce88bf5f3c3e57a7cdb7d
e-m-s-y <hofman.gb@gmail.com>
2021-09-17 10:40:54 +0200

    chore: removed dist folder

--- dist/service.d.ts
+++
@@ -1,7 +0,0 @@
-import { IOptions } from "./interfaces";
-export default class Service {
-    static readonly ID = "@foly/socket-event-forwarder";
-    private readonly app;
-    private server;
-    listen(options: IOptions): Promise<void>;
-}
